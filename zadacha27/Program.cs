fa83ea4d27ee421d0050784077c8fe1a540450b0
Fedor <fedor.kochkin48@gmail.com>
2022-12-15 00:58:24 +0300

    решение задачи 27

---
+++ zadacha27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int sum = 0;
+Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine());
+ while (a > 0)
+ {
+    sum = sum + a % 10;
+    a = a /10 ;
+ }
+ Console.WriteLine(sum);
