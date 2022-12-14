188cff76b85668dcb7626d78431096bc6012af21
Fedor <fedor.kochkin48@gmail.com>
2022-12-15 00:51:41 +0300

    решение задачи 25

---
+++ zadacha25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+int k = 1;
+Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите степень");
+int b = int.Parse(Console.ReadLine());
+for (int i = 1; i <= b; i++)
+{
+    k = k * a ;
+}
+Console.WriteLine(k);
