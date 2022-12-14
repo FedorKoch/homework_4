92d1a622f4a4047efda4a5781b066a7e7df5c01e
Fedor <fedor.kochkin48@gmail.com>
2022-12-15 01:17:26 +0300

    решение задачи 29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int b = 0;
+Console.WriteLine("введите количество элементов массива");
+int a = int.Parse(Console.ReadLine());
+int[] mas = new int[a];
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.WriteLine("введите элемент массива");
+     b = int.Parse(Console.ReadLine());
+   mas[i] = b;
+}
+var str = string.Join(" ", mas);
+Console.WriteLine(str);
